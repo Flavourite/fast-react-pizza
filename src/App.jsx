77eb296c8e28d597cbad15880815070fab97ae40
Favourite <effiongfavour57@gmail.com>
2025-09-12 08:42:54 +0100

    before using tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
 
@@ -28,10 +30,13 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
@@ -42,3 +47,20 @@ function App() {
 }
 
 export default App;
+// const tntLocation = {
+//   bba: 'Baba Adisa',
+//   scg: 'School Gate',
+//   abp: 'Abule Panu',
+//   ono: 'Onosa',
+//   iga: 'Igando',
+//   eko: 'Eleko',
+//   ala: 'Alahun',
+//   esk: 'Elesekan',
+//   sha: 'Shapatti',
+//   alt: 'Alatunshe',
+//   ade: 'Adeba',
+//   ori: 'Oribanwa',
+//   ogu: 'Ogunfayo',
+//   cop: 'Cooperative',
+//   off: 'Office',
+// };
